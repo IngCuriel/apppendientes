61aa8ffbefd9e06c3f69068c1417ad626cf59911
desarrolloCuriel <eleazar-00@hotmail.com>
2019-01-07 01:32:10 -0600

    Config package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app-pendiente'));
+app.use(express.static(__dirname + '/dist/AppPendientes'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/app-pendiente/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AppPendientes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
